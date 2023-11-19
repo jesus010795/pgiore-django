c024090f7671a32d35a6e74b48b0df8a88ef712e
jesus <jcd010795@gmail.com>
2023-11-19 14:38:11 -0600

    Creating simple views

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('company/', views.company, name='company'),
+    path('services/', views.services, name='services'),
+    path('products/', views.products, name='products'),
+    path('contact/', views.contact, name='contact'),
+    path('media/', views.media, name='media'),
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,23 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, template_name='core/home.html')
+
+
+def company(request):
+    return render(request, template_name='core/company.html')
+
+
+def services(request):
+    return render(request, template_name='core/services.html')
+
+
+def products(request):
+    return render(request, template_name='core/products.html')
+
+
+def contact(request):
+    return render(request, template_name='core/contact.html')
+
+
+def media(request):
+    return render(request, template_name='core/media.html')
